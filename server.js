745f73803a1fc660c2a58d9476d07c683da53bac
noelnme-gondry <gondry@users.noreply.github.com>
2026-03-19 09:02:26 +0900

    fix: express 5 wildcard routing error in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Send all other requests to index.html to allow SPA routing (if any)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
