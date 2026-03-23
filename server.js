c9e69367ab6e665b5762c0a8a6cf0ad7b7afef8f
noelnme-gondry <gondry@users.noreply.github.com>
2026-03-23 11:14:55 +0900

    chore: move ads.txt and robots.txt to root directory

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 3000;
 // Serve statically built Vite files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Serve ads.txt and robots.txt explicitly from root
+app.get('/ads.txt', (req, res) => res.sendFile(path.join(__dirname, 'ads.txt')));
+app.get('/robots.txt', (req, res) => res.sendFile(path.join(__dirname, 'robots.txt')));
+
 // Send all other requests to index.html to allow SPA routing (if any)
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
